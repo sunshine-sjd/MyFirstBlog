37812f47e17ab92e0a8c8740132afcd9d2185fbd
sunshine-sjd <xiaosarenzhe@163.com>
2016-08-20 14:34:39 +0800

    to utc

--- programe/app/main/__init__.py
+++ programe/app/main/__init__.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
-from datetime import datetime
 main = Blueprint('main', __name__)
-Now = datetime.now()
 from . import views, errors
 from ..models import Permission
 
 
 @main.app_context_processor
 def inject_permissions():
-    return dict(Permission=Permission, Now=Now)
+    return dict(Permission=Permission)
